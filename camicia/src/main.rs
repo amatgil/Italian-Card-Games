a06cb54c305edb825168a57e4f5ca5c358e63831
Amat Gil <amatgilvinyes@gmail.com>
2024-08-28 00:15:39 +0200

    Made camicia have a proper toggle for automatic mode (pretty graphs) or playing (very boring)

--- camicia/src/main.rs
+++ camicia/src/main.rs
@@ -2,23 +2,33 @@ use camicia::*;
 
 fn main() {
     let mut game = Game::new();
+    let mut rounds = 0;
+
+    const IS_AUTOMATIC: bool = true;  // Change at compile time
 
     let mut buffer = String::new();
     loop {
-        println!("{game}");
-        println!("Press the Any key for another move");
-        std::io::stdin().read_line(&mut buffer).expect("Could not read line from stdin");
+        if !IS_AUTOMATIC {
+            println!("{game}");
+            println!("Press the Any key for another move");
+            std::io::stdin().read_line(&mut buffer).expect("Could not read line from stdin");
+        }
         match game.is_over() {
             Some(winner) => {
-                println!("{winner:?} won! WOOO");
+                if !IS_AUTOMATIC { println!("{winner:?} won! WOOO. It took '{rounds}' rounds"); }
                 break;
             },
             None => game.tick(),
         }
-        let a = game.player_first.len();
-        let b = game.player_second.len();
-        let c = game.pile.len();
-        //println!("{} {} {}", a+b+c, b+a, c);
+
+        if IS_AUTOMATIC {
+            let a = game.player_first.len();
+            let b = game.player_second.len();
+            let c = game.pile.len();
+            println!("{} {} {}", a+b+c, b+a, c);
+        }
+
+        rounds += 1;
     }
 
 }
